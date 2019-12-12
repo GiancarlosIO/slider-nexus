5769ead5d67a9e847e627369a1bbd5a7d07880c4
Giancarlos <giancarlos.isasi@gmail.com>
2019-12-12 00:01:40 -0500

    Implement initial next and prev functions

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -16,21 +16,52 @@ const Inner = styled.div`
 `;
 
 const Slider = ({ children }) => {
-  const onNext = () => {};
+  const innerRef = React.useRef<HTMLDivElement>(null);
+  const [slideWidth, setSlideWidth] = React.useState(0);
+  const [currentIndex, setCurrentIndex] = React.useState(0);
 
-  const onPrev = () => {};
+  const childrens = React.Children.toArray(children);
+
+  const onNext = () => {
+    setCurrentIndex(index => {
+      if (currentIndex >= childrens.length - 1) {
+        return 0;
+      }
+      return index + 1;
+    });
+  };
+
+  const onPrev = () => {
+    if (currentIndex === 0) {
+      return;
+    }
+    setCurrentIndex(index => index - 1);
+  };
+
+  React.useLayoutEffect(() => {
+    if (innerRef.current) {
+      setSlideWidth(innerRef.current.getBoundingClientRect().width);
+    }
+  }, []);
+
+  console.log({ currentIndex, slideWidth });
 
   return (
     <div>
       <Container>
-        <Inner style={{ transform: `translate` }}>{children}</Inner>
+        <Inner
+          style={{ transform: `translateX(-${currentIndex * slideWidth}px)` }}
+          ref={innerRef}
+        >
+          {children}
+        </Inner>
       </Container>
       <div>
-        <button type="button" onClick={onNext}>
-          Next
-        </button>
         <button type="button" onClick={onPrev}>
           Prev
+        </button>
+        <button type="button" onClick={onNext}>
+          Next
         </button>
       </div>
     </div>
