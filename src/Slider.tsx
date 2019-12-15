f85d7c0234a49b0306f8fd47b967d0e07291fb93
Giancarlos <giancarlos.isasi@gmail.com>
2019-12-14 21:34:14 -0500

    Implement multi slider

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -9,21 +9,50 @@ const Container = styled.div`
 const Inner = styled.div`
   display: flex;
   flex-flow: row nowrap;
-  & > div {
-    min-width: 100%;
-  }
   transition: transform 0.3s ease;
 `;
 
 const Slider = ({ children }) => {
+  // const visibleElementIndex = React.useRef<number>(0);
   const innerRef = React.useRef<HTMLDivElement>(null);
-  const [slideWidth, setSlideWidth] = React.useState(0);
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const slideRef = React.useRef<HTMLDivElement>(null);
+
+  const [computedValues, setComputedValues] = React.useState({
+    containerWidth: 0,
+    itemWidth: 0,
+    gap: 0,
+  });
+  // const [innerSliderWidth, setInnerSlider] = React.useState(0);
   const [currentIndex, setCurrentIndex] = React.useState(0);
 
   const childrens = React.Children.toArray(children);
 
   const onNext = () => {
+    const visibleElments = Math.round(
+      computedValues.containerWidth / computedValues.itemWidth,
+    );
+
+    console.log({ visibleElments });
+
     setCurrentIndex(index => {
+      /**
+       * if we are showing multiples elements we need to reset the position if we are "showing" the last element
+       * ---
+       * Note that "showing" and "current" indexs are not the same
+       * "Showing" index is the last element that is viewed in the slider
+       * "Current" index is the index of the element tracked in the slider component itself
+       */
+      if (
+        visibleElments > 1 &&
+        visibleElments + currentIndex >= children.length
+      ) {
+        return 0;
+      }
+
+      /**
+       * If we are in the last slide, reset the index
+       */
       if (currentIndex >= childrens.length - 1) {
         return 0;
       }
@@ -39,21 +68,65 @@ const Slider = ({ children }) => {
   };
 
   React.useLayoutEffect(() => {
-    if (innerRef.current) {
-      setSlideWidth(innerRef.current.getBoundingClientRect().width);
+    // if (innerRef.current) {
+    //   setInnerSlider(innerRef.current.getBoundingClientRect().width);
+    // }
+
+    if (containerRef.current && slideRef.current) {
+      const itemMarginCSSRule = window
+        .getComputedStyle(slideRef.current, 'slide-nexus-slide')
+        .getPropertyValue('margin')
+        .split(' ');
+
+      const gap = parseInt(
+        (itemMarginCSSRule[1] || itemMarginCSSRule[3] || '0px').replace(
+          'px',
+          '',
+        ),
+        10,
+      );
+
+      setComputedValues({
+        itemWidth: slideRef.current.getBoundingClientRect().width,
+        containerWidth: containerRef.current.getBoundingClientRect().width,
+        gap,
+      });
     }
   }, []);
 
-  console.log({ currentIndex, slideWidth });
+  // visibleElementIndex.current = Math.round(
+  //   computedValues.containerWidth / computedValues.itemWidth,
+  // );
+  // We need to calculate what elements are visible!
+
+  console.log({
+    currentIndex,
+    // innerSliderWidth,
+    // visibleIndex: visibleElementIndex.current,
+    computedValues,
+  });
+
+  /**
+   * TODO: ADD CSS PREFIX TO TRAMSLATE CSS RULE
+   *
+   */
 
   return (
     <div>
-      <Container>
+      <Container ref={containerRef}>
         <Inner
-          style={{ transform: `translateX(-${currentIndex * slideWidth}px)` }}
+          style={{
+            transform: `translateX(-${currentIndex * computedValues.itemWidth +
+              currentIndex * computedValues.gap}px)`,
+          }}
           ref={innerRef}
         >
-          {children}
+          {childrens.map(childrenEl =>
+            React.cloneElement(childrenEl, {
+              ref: slideRef,
+              className: `${childrenEl.props.className} slide-nexus-item`,
+            }),
+          )}
         </Inner>
       </Container>
       <div>
